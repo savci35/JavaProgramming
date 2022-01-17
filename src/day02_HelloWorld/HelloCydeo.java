576d3f42a701f5ceb40873d2ab4a091af182315f
savci35 <ebrusavas2525@gmail.com>
2022-01-18 01:34:10 +0300

    Initial commit

---
+++ src/day02_HelloWorld/HelloCydeo.java
@@ -0,0 +1,20 @@
+package day02_HelloWorld;
+
+
+public class HelloCydeo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+        System.out.println("Wooden Spoon");
+        System.out.println("Java Development Compiler");
+        System.out.println("Java Runtime Enviranment");
+
+
+        System.out.println("Java Virtural Machine");
+    }
+
+}
+
+
+
---
+++ src/day02_HelloWorld/MyClassmates.java
@@ -0,0 +1,16 @@
+package day02_HelloWorld;
+
+public class MyClassmates {
+
+    public static void main(String[] args) {
+        System.out.println("Ebru Karıcım");
+        System.out.println("Savaş");
+        System.out.println("Barış");
+        System.out.println("Tuba");
+        System.out.println("Elif");
+
+
+    }
+
+
+}
