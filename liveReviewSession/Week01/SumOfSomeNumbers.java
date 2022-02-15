8ca100d21c9d3e443f2b8667ac93de91209bdec6
savci35 <ebrusavas2525@gmail.com>
2022-02-16 01:23:40 +0300

    16.02.2022

---
+++ liveReviewSession/Week01/SumOfSomeNumbers.java
@@ -0,0 +1,27 @@
+package Week01;
+
+import java.util.Scanner;
+
+public interface SumOfSomeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scan =new Scanner(System.in);
+
+        int min = 2147483647;
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("Enter a number :");
+            int num= scan.nextInt();
+
+
+            if (num<min){
+                min=num;
+            }
+
+        }
+
+        System.out.println("min="+min);
+    }
+}
